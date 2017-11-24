ff9bdc4d6e5633ebcfdcd6844a5603c6655b955f
Alexis Maerten <maerten.alexis@gmail.com>
2017-11-24 15:29:37 +0100

    debug2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 require 'json'
 require 'open-uri'
-
+ Drink.destroy_all
  Cocktail.destroy_all
  Ingredient.destroy_all
- Dose.destroy_all
+
 
 
 url= "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
