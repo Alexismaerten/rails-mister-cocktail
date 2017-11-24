4f81d301076ad3b397da54921f60e45eaae2165a
Alexis Maerten <maerten.alexis@gmail.com>
2017-11-24 17:57:00 +0100

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
  resources :cocktails, only:[:create, :index, :new, :show] do
  resources :doses, only:[:new, :create]
+ resources :reviews, only: [:new, :create]
 end
 resources :doses, only:[:destroy]
+
+
 
 root to: "cocktails#index"
 end
